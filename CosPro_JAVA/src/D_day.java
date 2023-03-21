0446a28a0890f8fb7322397f298bcb00480226ce
haeunNoh06 <okhagrace06@naver.com>
2023-03-21 16:58:56 +0900

    주어진 시작 날짜와 끝 날짜가 며칠만큼 떨어져 있는가를 구하기

---
+++ CosPro_JAVA/src/D_day.java
@@ -0,0 +1,31 @@
+public class D_day {
+	
+	public int func_a(int month, int day) {
+		int monthList[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+		int total = 0;
+		for ( int i = 1; i < month; i++) {
+			total += monthList[i-1];
+		}
+		total += day;
+		
+		
+		return total - 1;
+	}
+	
+	public int solution (int startMonth, int startDay, int endMonth, int endDay) {
+		int startTotal = func_a(startMonth, startDay);
+		int endTotal = func_a(endMonth, endDay);
+		return endTotal - startTotal;
+	}
+	
+	public static void main(String[] args) {
+		D_day d = new D_day();
+		int startMonth = 1;
+		int startDay = 2;
+		int endMonth = 2;
+		int endDay = 2;
+		int ret = d.solution(startMonth, startDay, endMonth, endDay);
+		System.out.println("Solution: return value of the method is "+ret+".");
+	}
+
+}
