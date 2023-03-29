70a7a24ea94bb845aa1737e9cb29d0da3746273e
haeunNoh06 <okhagrace06@naver.com>
2023-03-29 16:49:01 +0900

    토익 초급반 수강생 명수 알아내기

---
+++ CosPro_JAVA/src/ToeicClass.java
@@ -0,0 +1,22 @@
+
+public class ToeicClass {
+
+	public int solution(int[] score) {
+		int count = 0;
+		
+		for ( int i = 0; i < score.length; i++) {
+			if ( score[i] >= 650 && score[i] < 800 )
+				count++;
+		}
+		
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		ToeicClass t = new ToeicClass();
+		int[] scores = {650, 722, 914, 558, 714, 803, 650, 679, 669, 800};
+		int ret = t.solution(scores);
+		System.out.println("Solution: return value of the method is "+ret+".");
+	}
+
+}
