2b38db22802bcc7366fcfc7e280dc71b9a6d43de
haeunNoh06 <okhagrace06@naver.com>
2023-03-30 16:48:47 +0900

    문자열에서 연속하는 중복 문자 삭제

---
+++ CosPro_JAVA/src/DuplicateChar.java
@@ -0,0 +1,23 @@
+
+public class DuplicateChar {
+
+	public String solution(String characters) {
+		String result = "";
+		result += characters.charAt(0);
+		System.out.println(characters.length());
+		for ( int i = 0; i < characters.length()-1; i++) {
+			if ( characters.charAt(i) != characters.charAt(i+1))
+				result += characters.charAt(i+1);
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		DuplicateChar d = new DuplicateChar();
+		String characters = "senteeeeeenccccccccccccceeeeeee";
+		String ret = d.solution(characters);
+		
+		System.out.println("Solution: return value of the method is "+ret);
+	}
+
+}
