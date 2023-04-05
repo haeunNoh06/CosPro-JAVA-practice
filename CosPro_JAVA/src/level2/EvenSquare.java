3feb95d76943ab0d1375e8846ca20c1c4c41eaa4
haeunNoh06 <okhagrace06@naver.com>
2023-04-05 16:57:46 +0900

    m과 n 사이의 숫자 중 짝수의 제곱의 합을 구하기

---
+++ CosPro_JAVA/src/level2/EvenSquare.java
@@ -0,0 +1,25 @@
+package level2;
+
+public class EvenSquare {
+
+	public int solution(int N, int M) {
+		int total = 0;
+		
+		for ( int i = Math.min(N, M); i <= Math.max(N, M); i++) {
+			if ( i % 2 == 0)
+				total += i*i;
+		}
+		
+		return total;
+	}
+	
+	public static void main(String[] args) {
+		EvenSquare e = new EvenSquare();
+		int N = 4;
+		int M = 7;
+		int ret = e.solution(N, M);
+		
+		System.out.println("Solution : "+ret+".");
+	}
+
+}
