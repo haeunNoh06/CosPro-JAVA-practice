2d61b5ab8643c2356edd81633921cdab2cd9eb7f
haeunNoh06 <okhagrace06@naver.com>
2023-04-06 17:08:28 +0900

    엘리베이터 이동거리 구하기 Math.abs 사용한 버전

--- CosPro_JAVA/src/level2/Elevator.java
+++ CosPro_JAVA/src/level2/Elevator.java
@@ -5,10 +5,11 @@ public class Elevator {
         int dist = 0;
         int length = floors.length;
         for(int i = 1; i < floors.length; ++i){
-            if( floors[i] >= floors[i-1] )
-                dist += floors[i] - floors[i-1];
-            else
-                dist += floors[i-1] - floors[i];
+//            if( floors[i] >= floors[i-1] )
+//                dist += floors[i] - floors[i-1];
+//            else
+//                dist += floors[i-1] - floors[i];
+            dist += java.lang.Math.abs(floors[i]-floors[i-1]);
         }
         return dist;
     }
